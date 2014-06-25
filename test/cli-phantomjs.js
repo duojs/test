9f96a0d3268058b0a5632fa954dbfb08a3b731e7
Amir Abushareb <yields@icloud.com>
2014-06-25 17:59:26 +0300

    duo-test(1): test with phantomjs when no command is given

--- test/cli-phantomjs.js
+++ test/cli-phantomjs.js
@@ -3,14 +3,26 @@ var command = require('./support').command;
 var assert = require('assert');
 
 describe('cli - phantomjs', function(){
-  it('should fail', function*(){
+  it('should fail +phantomjs', function*(){
     var ret = yield command('duo-test', '--path test/fixtures/simple-fail/test phantomjs');
     assert(~ret.out.indexOf('got : false'), 'expected stdout to include "got : false"');
     assert.equal(1, ret.code);
   });
 
-  it('should succeed', function*(){
+  it('should fail -phantomjs', function*(){
+    var ret = yield command('duo-test', '--path test/fixtures/simple-fail/test');
+    assert(~ret.out.indexOf('got : false'), 'expected stdout to include "got : false"');
+    assert.equal(1, ret.code);
+  });
+
+  it('should succeed +phantomjs', function*(){
     var ret = yield command('duo-test', '--path test/fixtures/simple-success/test phantomjs');
+    assert(~ret.out.indexOf('should succeed'), 'expected stdout to include "should succeed"');
+    assert.equal(0, ret.code);
+  })
+
+  it('should succeed -phantomjs', function*(){
+    var ret = yield command('duo-test', '--path test/fixtures/simple-success/test');
     assert(~ret.out.indexOf('should succeed'), 'expected stdout to include "should succeed"');
     assert.equal(0, ret.code);
   })
