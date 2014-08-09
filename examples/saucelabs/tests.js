179dc7af752dbba7de985ab03106373aaf22e38e
Amir Abushareb <yields@icloud.com>
2014-08-09 21:52:46 +0300

    add saucelabs example

---
+++ examples/saucelabs/tests.js
@@ -0,0 +1,9 @@
+
+describe('saucelabs', function(){
+  it('should succeed', function(){});
+  it.skip('should skip', function(){});
+  it('should timeout', function(done){ setTimeout(done, 101); });
+  it('should error', function(){
+    throw new Error('err');
+  });
+});
