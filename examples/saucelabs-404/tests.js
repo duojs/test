cac7666c2e01c71e074b9e78dde6e3dbbba98fe9
Amir Abushareb <yields@icloud.com>
2014-08-10 16:52:17 +0300

    examples: removeem for now

--- examples/saucelabs-404/tests.js
+++
@@ -1,9 +0,0 @@
-
-describe('saucelabs - 404', function(){
-  it('should succeed', function(){});
-  it.skip('should skip', function(){});
-  it('should timeout', function(done){ setTimeout(done, 101); });
-  it('should error', function(){
-    throw new Error('err');
-  });
-});
