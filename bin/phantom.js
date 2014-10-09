b6a900d32467d97b45cd50b2d55a3a4877064aa0
Amir Abushareb <yields@icloud.com>
2014-10-09 20:47:27 +0300

    phantomjs: handle fatal errors

--- bin/phantom.js
+++ bin/phantom.js
@@ -1,5 +1,11 @@
 
 /**
+ * Module dependencies.
+ */
+
+var co = require('co');
+
+/**
  * Command
  */
 
@@ -12,8 +18,19 @@ module.exports = function*(cmd, dt){
 
   dt.on('browser', function(browser){
     new Reporter(browser.runner);
+
     browser.runner.once('end', function(obj){
       failures += obj.failures;
+    });
+
+    browser.runner.once('fatal', function(obj){
+      console.log('\033[31m%s\033[0m', obj.message);
+      console.log('  \033[90murl: %s', obj.url);
+      console.log('  lineno: %s\033[0m', obj.lineno);
+      console.log();
+      co(dt.destroy())(function(err){
+        process.exit(1);
+      });
     });
   });
 
